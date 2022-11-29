fbb881d5bd33d207e66903c0096f30810d47d3be
Naryond <giuggey85@gmail.com>
2022-11-29 02:17:04 +0000

    .

--- components/about.tsx
+++ components/about.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
 const About = () => {
-  return <div></div>;
+  return (
+    <div className="d-md-flex">
+      <div className="row mt-5 justify-content-around align-items-center">
+        <p className="col">
+          I am a self taught Front Edn Developer. I have had the help of a
+          mentor for several months. I focussed on learning JavaScript’s
+          fundamentals and moving primarily to React. With it I build a few
+          projects, mostly on Next JS, that involve the use and understanding of
+          HTML, CSS, Bootstrap, TypeScript, Firebase, React-Hook-Form, Node and
+          some other functional tools such GitHub and Trello.
+        </p>
+        <p>
+          I am also determined to grow as a developer and as value for the
+          company. I will keep studying the frameworks I am going to work with
+          for better understanding and knowledge and the new technologies that
+          might be required using in order to perform the tasks assigned.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,30 +5,30 @@ import About from '../components/about';
 
 const Home: NextPage = () => {
   return (
-    <>
+    <div className="p-5">
       <Head>
         <title>Giuseppe Valeriani</title>
       </Head>
-      <header className="header" id="home">
+      <header className="my-5" id="home">
         <article className="lead">
           <p className="text-center">
             I am a Front End Developer based in London (U.K.)
           </p>
           <p className="text-center">
-            I can use{' '}
-            <strong>ReactJS, JavaScript, HTML, CSS, TypeScript</strong>, but I
-            can't wait to expand my knowledge.
+            I can use
+            <strong> ReactJS, JavaScript, HTML, CSS, TypeScript </strong>
+            and I can't wait to expand my knowledge.
           </p>
         </article>
       </header>
-      <h3 className="center-section">Here some of my projects</h3>
+      <h3 className="text-center mb-5">Here some of my projects</h3>
       <section id="projects">
         <Projects />
       </section>
       <section id="about">
         <About />
       </section>
-    </>
+    </div>
   );
 };
 

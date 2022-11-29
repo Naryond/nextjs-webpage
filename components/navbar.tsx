00307a2237f6ab7c74b157bd5c1e9f30cd39e9b9
Naryond <giuggey85@gmail.com>
2022-11-29 01:06:27 +0000

    working navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,22 +6,25 @@ const NavBar = () => {
     <Navbar bg="light" expand="sm">
       <Container>
         <Navbar.Brand>Giuseppe Valeriani</Navbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#projects">Projects</Nav.Link>
-          <Nav.Link href="#about">About</Nav.Link>
-          <NavDropdown title="Contacts">
-            <NavDropdown.Item href="mailto:giuggey85@gmail.com">
-              Email
-            </NavDropdown.Item>
-            <NavDropdown.Item href="https://github.com/Naryond">
-              Git Hub
-            </NavDropdown.Item>
-            <NavDropdown.Item href="https://www.linkedin.com/in/giuseppe-valeriani-4b9995115/">
-              Linked In
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
+        <Navbar.Toggle aria-controls="navbarmenu" />
+        <Navbar.Collapse id="navbarmenu">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <NavDropdown title="Contacts">
+              <NavDropdown.Item href="mailto:giuggey85@gmail.com">
+                Email
+              </NavDropdown.Item>
+              <NavDropdown.Item href="https://github.com/Naryond">
+                Git Hub
+              </NavDropdown.Item>
+              <NavDropdown.Item href="https://www.linkedin.com/in/giuseppe-valeriani-4b9995115/">
+                Linked In
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );

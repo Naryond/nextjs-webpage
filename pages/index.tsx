0405dc6250b7fe876a4839300833611480ed1f98
Naryond <giuggey85@gmail.com>
2022-12-11 21:28:39 +0000

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,10 +12,10 @@ export type ProjectsType = {
     liveAppLink: string;
     technologies: string;
     _id: string;
-    _createdAt?: string;
-    _rev?: string;
-    _type?: string;
-    _updatedAt?: string;
+    // _createdAt?: string;
+    // _rev?: string;
+    // _type?: string;
+    // _updatedAt?: string;
   }[];
 };
 

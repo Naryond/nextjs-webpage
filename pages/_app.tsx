98f565ec19d27ab849713c1a5f7f3b4ca0a87e50
Naryond <giuggey85@gmail.com>
2022-12-12 01:18:49 +0000

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import NavBar from '../components/navbar';
+import { SSRProvider } from 'react-aria';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SSRProvider>
       <NavBar />
       <Component {...pageProps} />
-    </>
+    </SSRProvider>
   );
 }

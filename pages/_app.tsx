f71a5212fe86f20ed429c0712ae8b67f7611b169
Naryond <giuggey85@gmail.com>
2022-12-14 01:55:51 +0000

    Fav. Ico. set properly

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <SSRProvider>
       <Head>
-        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link rel="icon" href="/favicon.ico" />
         <title>Giuseppe Valeriani</title>
       </Head>
       <NavBar />
